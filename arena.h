c3ede850b8ceb4d4592c88f25ea91d638d778cf3
Christopher Milan <chrismilan@ucla.edu>
2025-07-31 11:42:12 -0400

    arena: free

--- arena.h
+++ arena.h
@@ -1,4 +1,4 @@
-/* arena.h - v0.1
+/* arena.h - v0.2
  * An arena allocator.
  *
  * Copyright (c) 2025 Christopher Milan
@@ -18,12 +18,10 @@
 #ifdef ARENA_IMPLEMENTATION
 #ifndef ARENA_DEFINITIONS
 #define ARENA_DEFINITIONS
-#include <unistd.h>
-#include <sys/mman.h>
-#define ARENA_REGION_SIZE sysconf(_SC_PAGESIZE)
-#define ARENA_REGION_ALLOC(r,sz) (r = mmap(0, sz, PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0)) == MAP_FAILED
-#define ARENA_REGION_FREE(r,sz) munmap(r, sz)
-
+#include <stdlib.h>
+#define ARENA_FREE(x)      free(x)
+#define ARENA_MALLOC(x)    malloc(x)
+#define ARENA_REALLOC(p,x) realloc(p,x)
 #endif
 #endif
 #ifndef _ARENA_H
@@ -34,61 +32,78 @@ typedef struct arena * arena_t;
 
 arena_t arena_create(void);
 void    arena_destroy(arena_t a);
-void   *arena_malloc(arena_t a, size_t size);
-void   *arena_zalloc(arena_t a, size_t size);
+void   *arena_malloc(arena_t a, size_t n);
+void   *arena_zalloc(arena_t a, size_t n);
+void   *arena_realloc(arena_t a, void *p, size_t n);
+void    arena_free(arena_t a, void *p);
+
+char *arena_strdup(arena_t a, const char *s);
 
 #endif
 #ifdef ARENA_IMPLEMENTATION
-#include <errno.h>
-#include <stdlib.h>
 #include <string.h>
 
-typedef struct region {
-  struct region *next;
-  size_t cnt, cap;
-  uintptr_t data[];
-}* region_t;
-
 struct arena {
-  region_t head, tail;
+  struct block {
+    struct block *prev, *next;
+  } *head;
 };
 
-static inline struct region *create_region(void) {
-  region_t r;
-  if (ARENA_REGION_ALLOC(r, ARENA_REGION_SIZE)) return 0;
-  memset(r, 0, sizeof(*r));
-  r->cap = (ARENA_REGION_SIZE - sizeof(*r)) / sizeof(uintptr_t);
-  return r;
-}
-
 arena_t arena_create(void) {
-  return memset(malloc(sizeof(struct arena)), 0, sizeof(struct arena));
+  arena_t a = (arena_t)ARENA_MALLOC(sizeof(struct arena));
+  a->head = 0;
+  return a;
 }
 
 void arena_destroy(arena_t a) {
-  for (region_t r = a->head; r; r = r->next) ARENA_REGION_FREE(r, ARENA_REGION_SIZE);
-  free(a);
+  struct block *b;
+  while ((b = a->head)) {
+    a->head = b->next;
+    ARENA_FREE(b);
+  }
+  ARENA_FREE(a);
 }
 
-void *arena_malloc(arena_t a, size_t size) {
-  void *ret;
-  size_t sz = (size + sizeof(uintptr_t) - 1) / sizeof(uintptr_t);
-  region_t r = a->head;
-  if (size > (size_t)ARENA_REGION_SIZE) { errno = E2BIG; return 0; }
-  if (!a->head || a->head->cnt + sz > a->head->cap) {
-    a->head = create_region();
-    a->head->next = r;
-    if (!a->tail) a->tail = a->head;
-  } else for (;r->next && r->next->cnt + sz > a->head->cap; r = r->next);
-  ret = &r->data[r->cnt];
-  r->cnt += sz;
+void *arena_malloc(arena_t a, size_t n) {
+  struct block *b;
+  if (!(b = ARENA_MALLOC(sizeof(struct block) + n))) return 0;
+  b->prev = 0;
+  b->next = a->head;
+  a->head = b;
+  return b + 1;
+}
+
+void *arena_zalloc(arena_t a, size_t n) {
+  void *ret = arena_malloc(a, n);
+  if (ret) memset(ret, 0, n);
   return ret;
 }
 
-void *arena_zalloc(arena_t a, size_t size) {
-  void *ret = arena_malloc(a, size);
-  if (ret) memset(ret, 0, size);
-  return ret;
+void *arena_realloc(arena_t a, void *p, size_t n) {
+  struct block *b = (struct block *)p - 1, *nb;
+  if (!p) return arena_malloc(a, n);
+  if (!(nb = ARENA_REALLOC(b, sizeof(sizeof(struct block) + n)))) return 0;
+  if (b != nb) {
+    if (nb->prev) nb->prev = nb;
+    else a->head = nb;
+    if (nb->next) nb->next->prev = nb;
+  }
+  return nb + 1;
+}
+
+void arena_free(arena_t a, void *p) {
+  struct block *b = (struct block *)p - 1;
+  if (b->prev) b->prev->next = b->next;
+  else a->head = b->next;
+  if (b->next) b->next->prev = b->prev;
+  ARENA_FREE(b);
+}
+
+char *arena_strdup(arena_t a, const char *s) {
+  char *d;
+  if (s == NULL) return NULL;
+  if ((d = arena_malloc(a, strlen(s) + 1))) strcpy(d, s);
+  return d;
 }
 
 #endif
